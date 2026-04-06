373865751808e46c1242171a78336dc84994fa97
Ad1tyzzRamadhani <adityafr79@gmail.com>
2026-04-06 14:30:53 +0700

    Create parser.cpp

---
+++ parser.cpp
@@ -0,0 +1,23 @@
+struct ParseState {
+    const std::vector<Token>& tokens;
+    size_t pos = 0;
+
+    const Token& peek(int offset = 0) const {
+        if (pos + offset >= tokens.size()) return tokens.back();
+        return tokens[pos + offset];
+    }
+
+    const Token& advance() { return tokens[pos++]; }
+
+    bool match(TokenType t) {
+        if (peek().type == t) { advance(); return true; }
+        return false;
+    }
+
+    [[noreturn]] void error(const std::string& msg) const {
+        const auto& tok = peek();
+        throw std::runtime_error(tok.file + ":" +
+                                 std::to_string(tok.line) + ":" +
+                                 std::to_string(tok.column) + " " + msg);
+    }
+};
