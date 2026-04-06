6ffbdc0e3d6d18035230fb50e3f9bd771de98e64
Ad1tyzzRamadhani <adityafr79@gmail.com>
2026-04-06 15:59:50 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,11 @@
+#include "directivelinker.cpp"
+#include "lexer.cpp"
+#include "ast.hpp"
+#include "parser.cpp"
+#include "parser_literal.cpp"
+#include "parser_primary_expr.cpp"
+#include "parser_postfix_expr.cpp"
+
 int main() {
   return 0;
 }
