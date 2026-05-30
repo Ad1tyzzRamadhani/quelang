85da587be98fa7b6b72b0c7f5809c757ea19ff96
Ad1tyzzRamadhani <adityafr79@gmail.com>
2026-05-30 22:37:02 +0700

    Create parser_type.cpp

---
+++ parser_type.cpp
@@ -0,0 +1,114 @@
+std::unique_ptr<Type> parseType() {
+    auto type = std::make_unique<Type>();
+    while (true) {
+        if (match(TokenType::KW_CONST)) {
+            type->qualifiers.push_back(
+                TypeQualifier::Const
+            );
+        }
+        else if (match(TokenType::KW_VOLATILE)) {
+            type->qualifiers.push_back(
+                TypeQualifier::Volatile
+            );
+        }
+        else {
+            break;
+        }
+    }
+
+    type->base = parseQualifiedName(tokens, pos);
+    while (true) {
+
+        // pointer
+        if (match(TokenType::STAR)) {
+            TypeModifier mod;
+            mod.kind = TypeModifier::Kind::Pointer;
+
+            type->modifiers.push_back(
+                std::move(mod)
+            );
+
+            continue;
+        }
+
+        // reference
+        if (match(TokenType::AMP)) {
+            TypeModifier mod;
+            mod.kind = TypeModifier::Kind::Reference;
+
+            type->modifiers.push_back(
+                std::move(mod)
+            );
+
+            continue;
+        }
+
+        // function pointer
+        if (peek().type == TokenType::LPAREN) {
+
+            size_t backup = pos;
+
+            try {
+
+                advance(); // (
+
+                std::vector<std::unique_ptr<Type>> params;
+
+                if (!check(TokenType::RPAREN)) {
+
+                    do {
+                        params.push_back(
+                            parseType()
+                        );
+                    }
+                    while (match(TokenType::COMMA));
+                }
+
+                consume(
+                    TokenType::RPAREN,
+                    "expected ')' in function pointer"
+                );
+
+                if (
+                    check(TokenType::STAR) ||
+                    check(TokenType::AMP)
+                ) {
+
+                    TypeModifier mod;
+                    mod.kind =
+                        TypeModifier::Kind::FuncPtr;
+
+                    mod.func_params =
+                        std::move(params);
+
+                    if (match(TokenType::STAR)) {
+                        // func ptr
+                    }
+                    else {
+                        consume(
+                            TokenType::AMP,
+                            "expected * or & after function type"
+                        );
+                    }
+
+                    type->modifiers.push_back(
+                        std::move(mod)
+                    );
+
+                    continue;
+                }
+
+                pos = backup;
+                break;
+            }
+            catch (...) {
+                pos = backup;
+                break;
+            }
+        }
+
+        break;
+    }
+
+    return type;
+}
