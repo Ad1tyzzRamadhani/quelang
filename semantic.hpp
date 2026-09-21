1b3b9a51ee12d462d16d545a99ee47d479807266
Ad1tyzzRamadhani <adityafr79@gmail.com>
2026-09-21 16:47:03 +0700

    Update semantic.hpp

--- semantic.hpp
+++ semantic.hpp
@@ -117,6 +117,7 @@ private:
     TypeView view(const std::unique_ptr<Type>& type) const;
     std::string typeString(const TypeView& type) const;
     std::string typeString(Type* type) const;
+    bool canConvert(const TypeView& from, const TypeView& to);
     bool sameType(const TypeView& a, const TypeView& b) const;
     bool sameType(Type* a, Type* b) const;
     bool isBuiltin(const std::string& name) const;
