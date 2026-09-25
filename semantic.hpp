cc2af4a0e8ef9c10e37783bba2ec0caa389ce4c6
Ad1tyzzRamadhani <adityafr79@gmail.com>
2026-09-25 15:12:58 +0700

    Update semantic.hpp

--- semantic.hpp
+++ semantic.hpp
@@ -45,14 +45,14 @@ private:
     struct TypeView;
     struct FunctionTypeView {
         std::vector<TypeView> parameters;
-        TypeView return_type;
+        TypeView* return_type = nullptr;
         bool is_coroutine = false;
     };
     struct TypeView {
         std::string base;
         std::vector<TypeModifier::Kind> modifiers;
         std::vector<TypeQualifier> qualifiers;
-        FunctionTypeView* function = nullptr;
+        FunctionTypeView& function;
         bool is_coroutine = false;
         bool valid = false;
         bool unknown = false;
