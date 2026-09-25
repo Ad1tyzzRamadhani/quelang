6c028528bef1b486d15d73cba5decf81fa7fdad2
Ad1tyzzRamadhani <adityafr79@gmail.com>
2026-09-25 16:37:05 +0700

    Update semantic.cpp

--- semantic.cpp
+++ semantic.cpp
@@ -1768,8 +1768,8 @@ std::cout << "CURRENT MODIFIERS: "
                     error(expr, "expression is not callable");
                     return {};
                 }
-                ret.function->return_type = view(pending_function->return_types.get());
-                ret.function->throws_type = view(pending_function->throws_type.get());
+                ret.function->return_type = &pending_function->return_types.get();
+                ret.function->throws_type = &pending_function->throws_type.get();
                 ret.function->is_coroutine = pending_function->is_coroutine;
                 current = ret;
                 pending_symbol = nullptr;
