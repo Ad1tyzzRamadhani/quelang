4e0dcaaa7afb029043bd63aed5e2aca27b49c65d
Ad1tyzzRamadhani <adityafr79@gmail.com>
2026-09-25 15:40:07 +0700

    Update semantic.cpp

--- semantic.cpp
+++ semantic.cpp
@@ -1710,8 +1710,7 @@ std::cout << "CURRENT MODIFIERS: "
                         mod.func_params.push_back(p.type);
                     }
                     mod.is_coroutine = true;
-                    auto throws_type = pending_function->throws_type;
-                    mod.throws_type = &throws_type;
+                    mod.throws_type = std::make_unique<Type>(pending_function->throws_type);
                     pending_function->return_types->modifiers = std::move(mod);
                 }
                 if (pending_function) {
